82a3180081e3eaf85096ae4124ea10de3391ebf7
Kirk Kohler <kirkkohler@hotmail.com>
2014-02-05 23:15:11 -0800

    committing initial angular-yo generated code.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('kirkkohlerAngularYoWebsiteApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
